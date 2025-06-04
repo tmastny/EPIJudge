2e5e1f95132950267936f94d19d0f9834b527e9c
Tim Mastny <tim.mastny@gmail.com>
2025-06-04 17:47:42 -0500

    swap bits

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -2,11 +2,44 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    if i > j:
+        i, j = j, i
+
+    diff = abs(i - j)
+
+    bit_i = x & (1 << i)
+    x &= ~bit_i
+    bit_i <<= diff
+
+    bit_j = x & (1 << j)
+    x &= ~bit_j
+    bit_j >>= diff
+
+    return x | bit_i | bit_j
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('swap_bits.py', 'swap_bits.tsv',
-                                       swap_bits))
+def swap_bits_flip(x, i, j):
+    bit_i = x & (1 << i)
+    bit_j = x & (1 << j)
+    
+    swap = (bit_i >> i) ^ (bit_j >> j)
+    x ^= (swap << i) ^ (swap << j)
+    
+    return x
+
+
+def swap_bits_book(x, i, j):
+    bit_i = (x >> i) & 1 
+    bit_j = (x >> j) & 1
+    
+    swap = bit_i ^ bit_j
+    x ^= (swap << i) ^ (swap << j)
+    
+    return x
+              
+
+
+if __name__ == "__main__":
+    generic_test.generic_test_main("swap_bits.py", "swap_bits.tsv", swap_bits)
+    generic_test.generic_test_main("swap_bits.py", "swap_bits.tsv", swap_bits_flip)
+    generic_test.generic_test_main("swap_bits.py", "swap_bits.tsv", swap_bits_book)
