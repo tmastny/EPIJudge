496c0e3232bfd6fba273836281c2bef8e6cc6bc7
Tim Mastny <tim.mastny@gmail.com>
2025-06-06 06:53:32 -0500

    weight

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
 
+def closet_int_same_bit_count_iterative(x: int) -> int:
+    for i in range(64):
+        if (x >> i) & 1 != (x >> (i + 1)) & 1:
+            return x ^ ((1 << i) | 1 << (i + 1))
+            
+    
+    return -1
+
 
 def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    lsb1 = x ^ (x & (x - 1))
+    lsb0 = ~x ^ (~x & (~x - 1))
+    
+    if lsb1 > lsb0:
+        swap_mask = lsb1 | (lsb1 >> 1)
+    else:
+        swap_mask = lsb0 | (lsb0 >> 1)
+        
+    return x ^ swap_mask
 
 
 if __name__ == "__main__":
@@ -12,3 +27,9 @@ if __name__ == "__main__":
         "closest_int_same_weight.tsv",
         closest_int_same_bit_count,
     )
+    generic_test.generic_test_main(
+        "closest_int_same_weight.py",
+        "closest_int_same_weight.tsv",
+        closet_int_same_bit_count_iterative,
+    )
+
