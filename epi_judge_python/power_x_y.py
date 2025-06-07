b56b0dc8ed6d09b78cf0fa9d43d4d2922e7bc229
Tim Mastny <tim.mastny@gmail.com>
2025-06-07 17:24:35 -0500

    power

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,10 +2,18 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    positive = y >= 0
+    y = abs(y)
+    
+    pow = 1
+    while y:
+        if y & 1:
+            pow *= x
+        x *= x
+        y >>= 1
+
+    return pow if positive else 1 / pow
 
 
-if __name__ == '__main__':
-    exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
-                                        power))
+if __name__ == "__main__":
+    generic_test.generic_test_main("power_x_y.py", "power_x_y.tsv", power)
