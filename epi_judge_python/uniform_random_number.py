242a83c1e356a82a0100a8eda60676215aed2562
Tim Mastny <tim.mastny@gmail.com>
2025-06-09 11:25:04 -0500

    uniform random

--- epi_judge_python/uniform_random_number.py
+++ epi_judge_python/uniform_random_number.py
@@ -3,7 +3,9 @@ import random
 
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
-    check_sequence_is_uniformly_random, run_func_with_retries)
+    check_sequence_is_uniformly_random,
+    run_func_with_retries,
+)
 from test_framework.test_utils import enable_executor_hook
 
 
@@ -12,28 +14,38 @@ def zero_one_random():
 
 
 def uniform_random(lower_bound: int, upper_bound: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    ub = upper_bound - lower_bound
+
+    rand = ub + 1
+    while rand > ub:
+        rand = 0
+        for pow2 in range(ub.bit_length()):
+            rand |= (zero_one_random() << pow2)
+    
+    return lower_bound + rand
 
 
 @enable_executor_hook
 def uniform_random_wrapper(executor, lower_bound, upper_bound):
     def uniform_random_runner(executor, lower_bound, upper_bound):
         result = executor.run(
-            lambda:
-            [uniform_random(lower_bound, upper_bound) for _ in range(100000)])
+            lambda: [uniform_random(lower_bound, upper_bound) for _ in range(100000)]
+        )
 
         return check_sequence_is_uniformly_random(
-            [a - lower_bound for a in result], upper_bound - lower_bound + 1,
-            0.01)
+            [a - lower_bound for a in result], upper_bound - lower_bound + 1, 0.01
+        )
 
     run_func_with_retries(
-        functools.partial(uniform_random_runner, executor, lower_bound,
-                          upper_bound))
+        functools.partial(uniform_random_runner, executor, lower_bound, upper_bound)
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('uniform_random_number.py',
-                                       'uniform_random_number.tsv',
-                                       uniform_random_wrapper))
+        generic_test.generic_test_main(
+            "uniform_random_number.py",
+            "uniform_random_number.tsv",
+            uniform_random_wrapper,
+        )
+    )
