27ec1292e5949faf144d4d03c6cd2fca8a921976
Tim Mastny <tim.mastny@gmail.com>
2025-06-10 10:17:50 -0500

    epi_judge_python/sorted_arrays_merge.py

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -2,10 +2,26 @@ from typing import List
 
 from test_framework import generic_test
 
+import heapq
+
+from collections import namedtuple
+
+minArray = namedtuple('MinArray', ['min', 'idx', 'array'])
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    heap = [minArray(array[0], 0, array) for array in sorted_arrays]
+    heapq.heapify(heap)
+
+    merged = []
+    while heap:
+        min, idx, array = heapq.heappop(heap)
+        merged.append(min)
+
+        idx += 1
+        if idx < len(array):
+            heapq.heappush(heap, minArray(array[idx], idx, array))
+
+    return merged
 
 
 if __name__ == '__main__':
