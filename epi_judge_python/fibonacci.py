327edbfd539a4b5e52e5b6acd8f238f6ef5406a0
Tim Mastny <tim.mastny@gmail.com>
2025-06-14 13:42:39 -0500

    epi_judge_python/fibonacci.py

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
 
+def fibonacci_space_n(n: int) -> int:
+    dp = [0] * ((n + 1) + 2)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    
+    return dp[n]
+
 def fibonacci(n: int) -> int:
-    # TODO - you fill in here.
-    return -1
+    if n == 0:
+        return 0
+    
+    prev = 0
+    curr = 1
+    for i in range(2, n + 1):
+        next_prev = curr
+        curr += prev
+        prev = next_prev
+         
+    return curr
+
 
 
 if __name__ == '__main__':
