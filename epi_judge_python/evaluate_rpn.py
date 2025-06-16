7b41b3abcb8b455e9e4d8309866a092e6a634f14
Tim Mastny <tim.mastny@gmail.com>
2025-06-16 14:55:39 -0500

    epi_judge_python/evaluate_rpn.py

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,8 +2,30 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack = []
+    i = 0
+    while i < len(expression):
+        token = []
+        while i < len(expression) and expression[i] != ",":
+            token.append(expression[i])
+            i += 1
+        i += 1
+        token = "".join(token)
+
+        if token in "+-*/":
+            arg2 = stack.pop()
+            if token == "+":
+                stack[-1] += arg2
+            elif token == "-":
+                stack[-1] -= arg2
+            elif token == "*":
+                stack[-1] *= arg2
+            elif token == "/":
+                stack[-1] //= arg2
+        else:
+            stack.append(int(token))
+
+    return stack[-1]
 
 
 if __name__ == '__main__':
