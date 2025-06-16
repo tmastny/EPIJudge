7a53595d47f84641d4a6c402d59b0e98b40bda32
Tim Mastny <tim.mastny@gmail.com>
2025-06-16 14:54:29 -0500

    epi_judge_python/sorted_lists_merge.py

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -4,14 +4,29 @@ from list_node import ListNode
 from test_framework import generic_test
 
 
-def merge_two_sorted_lists(L1: Optional[ListNode],
-                           L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+def merge_two_sorted_lists(
+    L1: Optional[ListNode], L2: Optional[ListNode]
+) -> Optional[ListNode]:
+    head = tail = ListNode()
+    while L1 and L2:
+        if L1.data < L2.data:
+            tail.next = L1
+            L1 = L1.next
+        else:
+            tail.next = L2
+            L2 = L2.next
+        
+        tail = tail.next 
+
+    ll = L1 or L2
+    tail.next = ll
+
+    return head.next
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('sorted_lists_merge.py',
-                                       'sorted_lists_merge.tsv',
-                                       merge_two_sorted_lists))
+        generic_test.generic_test_main(
+            "sorted_lists_merge.py", "sorted_lists_merge.tsv", merge_two_sorted_lists
+        )
+    )
