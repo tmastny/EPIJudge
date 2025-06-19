d7c529ab2e581931c19ede5f55a4ac8b7f90e8ef
Tim Mastny <tim.mastny@gmail.com>
2025-06-19 14:19:00 -0500

    epi_judge_python/two_sorted_arrays_merge.py

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -3,10 +3,22 @@ from typing import List
 from test_framework import generic_test
 
 
-def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
-                            n: int) -> None:
-    # TODO - you fill in here.
-    return
+def merge_two_sorted_arrays(A: List[int], m: int, B: List[int], n: int) -> None:
+    i = m + n - 1
+    while m > 0 and n > 0:
+        if A[m - 1] > B[n - 1]:
+            A[i] = A[m - 1]
+            m -= 1
+        else:
+            A[i] = B[n - 1]
+            n -= 1
+        i -= 1
+
+    C, k = (A, m) if m > 0 else (B, n)
+    while k > 0:
+        A[i] = C[k - 1]
+        k -= 1
+        i -= 1
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
@@ -14,8 +26,11 @@ def merge_two_sorted_arrays_wrapper(A, m, B, n):
     return A
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('two_sorted_arrays_merge.py',
-                                       'two_sorted_arrays_merge.tsv',
-                                       merge_two_sorted_arrays_wrapper))
+        generic_test.generic_test_main(
+            "two_sorted_arrays_merge.py",
+            "two_sorted_arrays_merge.tsv",
+            merge_two_sorted_arrays_wrapper,
+        )
+    )
