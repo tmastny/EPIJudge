bc5b79b9d4b14547e44c546ce5e87fec511b5c19
Tim Mastny <tim.mastny@gmail.com>
2025-06-23 15:00:20 -0500

    epi_judge_python/int_as_array_increment.py

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -3,9 +3,35 @@ from typing import List
 from test_framework import generic_test
 
 
+def plus_one1(A: List[int]) -> List[int]:
+    carry = 1
+    for i in range(len(A) - 1, -1, -1):
+       sum = A[i] + carry
+       carry = sum // 10
+       A[i] = sum % 10
+
+       if not carry:
+           break
+
+    if carry:
+        A.insert(0, carry)
+
+    return A
+
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        
+        A[i] = 0
+        A[i - 1] += 1
+    
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    
+    return A
 
 
 if __name__ == '__main__':
