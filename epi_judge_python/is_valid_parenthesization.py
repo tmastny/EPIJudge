9fddd21f68ab3a32ff26f65f01e88aafda0b5ed2
Tim Mastny <tim.mastny@gmail.com>
2025-06-23 15:12:19 -0500

    epi_judge_python/is_valid_parenthesization.py

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+CLOSED_2_OPENED = {
+    "}": "{",
+    "]": "[",
+    ")": "("
+}
+
+def matches(opened, closed):
+    return opened == CLOSED_2_OPENED[closed]
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    stack = []
+    for i in range(len(s)):
+        if s[i] in "{([":
+            stack.append(s[i])
+        elif s[i] != ",":
+            if stack and matches(stack[-1], s[i]):
+                stack.pop()
+            else:
+                return False
+
+    return len(stack) == 0
 
 
 if __name__ == '__main__':
