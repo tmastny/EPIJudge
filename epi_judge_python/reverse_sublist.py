fbdfa2664b67d9a8c949028ec2b97de1be7800ea
Tim Mastny <tim.mastny@gmail.com>
2025-06-23 15:09:56 -0500

    epi_judge_python/reverse_sublist.py

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -4,13 +4,30 @@ from list_node import ListNode
 from test_framework import generic_test
 
 
-def reverse_sublist(L: ListNode, start: int,
-                    finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+def reverse_sublist(L: ListNode, start: int, finish: int) -> Optional[ListNode]:
+    n = 1
+    head = tail = ListNode()
+    tail.next = L
+
+    node = L
+    while node and n < start:
+        tail = node
+        node = node.next
+        n += 1
+
+    while n < finish:
+        nextnode = node.next
+        node.next = nextnode.next
+        nextnode.next = tail.next
+        tail.next = nextnode
+        n += 1
+
+    return head.next
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('reverse_sublist.py',
-                                       'reverse_sublist.tsv', reverse_sublist))
+        generic_test.generic_test_main(
+            "reverse_sublist.py", "reverse_sublist.tsv", reverse_sublist
+        )
+    )
