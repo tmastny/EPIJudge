0e65917474971333f5b19a5c5b847fccc6346601
Tim Mastny <tim.mastny@gmail.com>
2025-06-23 14:36:44 -0500

    epi_judge_python/matrix_connected_regions.py

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -4,7 +4,21 @@ from test_framework import generic_test
 
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
+    COLOR = image[x][y]
+    
+    def dfs(x, y):
+        if (
+            x < 0 or x >= len(image) or
+            y < 0 or y >= len(image[0]) or 
+            image[x][y] != COLOR
+        ):
+            return
+        
+        image[x][y] = not COLOR 
+        for nx, ny in [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]:
+            dfs(nx, ny)
+    
+    dfs(x, y)
     return
 
 
