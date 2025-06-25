bee35b1d713733f67506e2d5d3b37476d467140a
Tim Mastny <tim.mastny@gmail.com>
2025-06-25 15:26:20 -0500

    epi_judge_python/is_anonymous_letter_constructible.py

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,15 +1,25 @@
 from test_framework import generic_test
 
+from collections import Counter
 
-def is_letter_constructible_from_magazine(letter_text: str,
-                                          magazine_text: str) -> bool:
-    # TODO - you fill in here.
+def is_letter_constructible_from_magazine(letter_text: str, magazine_text: str) -> bool:
+    letter_freq = Counter(letter_text)
+    magazine_freq = Counter(magazine_text)
+    
+    for letter, count in letter_freq.items():
+        if letter in magazine_freq and magazine_freq[letter] >= count:
+            continue
+        
+        return False
+    
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            'is_anonymous_letter_constructible.py',
-            'is_anonymous_letter_constructible.tsv',
-            is_letter_constructible_from_magazine))
+            "is_anonymous_letter_constructible.py",
+            "is_anonymous_letter_constructible.tsv",
+            is_letter_constructible_from_magazine,
+        )
+    )
