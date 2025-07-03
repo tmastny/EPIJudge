65f8c7d11153d251da96ee58e58a2d8d7086dff9
Tim Mastny <tim.mastny@gmail.com>
2025-07-03 09:20:48 -0500

    epi_judge_python/advance_by_offsets.py

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -2,10 +2,36 @@ from typing import List
 
 from test_framework import generic_test
 
+from math import inf
+
+def can_reach_end_steps(A: List[int]) -> bool:
+    steps = -inf
+    for i, next_steps in enumerate(A):
+        if i == len(A) - 1:
+           return True
+
+        steps = max(steps, next_steps)
+        if steps == 0:
+            break
+
+        steps -= 1
+
+    return False
+
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    idx = 0
+    for i, steps in enumerate(A):
+        if i > idx:
+            break
+        
+        idx = max(idx, i + steps)
+        if idx >= len(A) - 1:
+            return True
+
+    return False
+
+
 
 
 if __name__ == '__main__':
