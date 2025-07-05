c46ee47e9f212d30cfcca135c017cd7cc5c7db60
Tim Mastny <tim.mastny@gmail.com>
2025-07-05 14:04:25 -0500

    epi_judge_python/directory_path_normalization.py

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,67 @@
 from test_framework import generic_test
 
 
+def shortest_equivalent_path1(path: str) -> str:
+    directories = []
+    name = ''
+    for s in path:
+        if s == "/":
+            if name != "":
+                directories.append(name)
+                name = ''
+        else:
+            name += s
+
+    if name != "":
+        directories.append(name)
+
+    stack = []
+    if path[0] == "/":
+        stack.append("/")
+        
+    for dir in directories:
+        if dir == ".":
+            continue
+        elif dir == ".." and stack:
+            if stack[-1] == "/":
+                continue
+            elif stack[-1] == ".":
+                stack[-1] = ".."
+            elif stack[-1] == "..":
+                stack.append(dir)
+            else:
+                stack.pop()
+        else:
+           stack.append(dir)
+    
+    normalized = "/".join(stack)
+    if path[0] == "/" and len(normalized) > 1:
+        return normalized[1:] 
+    
+    return normalized 
+
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    tokens = [token for token in path.split("/") if token not in [".", ""]]
+    
+    stack = []
+    if path[0] == "/":
+        stack.append("/")
+    
+    for token in tokens:
+        if token == "..":
+            if stack and stack[-1] != "..":
+                stack.pop()
+            else:
+                stack.append(token)
+        else:
+            stack.append(token)
+    
+    normalized = "/".join(stack)
+    if normalized.startswith("//"):
+        return normalized[1:]
+        
+    return normalized
+
 
 
 if __name__ == '__main__':
