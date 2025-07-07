131a053e850ced8494c48af3def0cd27aa5c1285
Tim Mastny <tim.mastny@gmail.com>
2025-07-07 14:56:20 -0500

    epi_judge_python/lowest_common_ancestor.py" "

--- epi_judge_python/lowest_common_ancestor.py
+++ epi_judge_python/lowest_common_ancestor.py
@@ -8,26 +8,65 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def lca(tree: BinaryTreeNode, node0: BinaryTreeNode,
-        node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+def lca0(
+    tree: BinaryTreeNode | None, node0: BinaryTreeNode, node1: BinaryTreeNode
+) -> Optional[BinaryTreeNode]:
+    if not tree:
+        return None
+    elif tree.data == node0.data or tree.data == node1.data:
+        return tree 
+        
+    left = lca(tree.left, node0, node1)
+    right = lca(tree.right, node0, node1)
+    if left and right:
+        return tree
+    
+    return left or right
+
+def lca(
+    tree: BinaryTreeNode | None, node0: BinaryTreeNode, node1: BinaryTreeNode
+) -> Optional[BinaryTreeNode]:
+    if not tree:
+        return None
+    
+    def dfs(tree, node0, node1):
+        if not tree:
+            return (0, None)
+        
+        lcount, lnode = dfs(tree.left, node0, node1)
+        if lcount == 2:
+            return (lcount, lnode)
+        
+        rcount, rnode = dfs(tree.right, node0, node1)
+        if rcount == 2:
+            return (rcount, rnode)
+        
+        count = lcount + rcount + int(tree.data == node0.data) + int(tree.data == node1.data)
+        return (count, tree if count == 2 else None)
+        
+    
+    _, lca_node = dfs(tree, node0, node1)
+    return lca_node
+
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, key1, key2):
     strip_parent_link(tree)
     result = executor.run(
-        functools.partial(lca, tree, must_find_node(tree, key1),
-                          must_find_node(tree, key2)))
+        functools.partial(
+            lca, tree, must_find_node(tree, key1), must_find_node(tree, key2)
+        )
+    )
 
     if result is None:
-        raise TestFailure('Result can\'t be None')
+        raise TestFailure("Result can't be None")
     return result.data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('lowest_common_ancestor.py',
-                                       'lowest_common_ancestor.tsv',
-                                       lca_wrapper))
+        generic_test.generic_test_main(
+            "lowest_common_ancestor.py", "lowest_common_ancestor.tsv", lca_wrapper
+        )
+    )
--- epi_judge_python/lru_cache.py
+++ epi_judge_python/lru_cache.py
@@ -2,6 +2,57 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
+class ListNode:
+    def __init__(
+        self,
+        key: int = -1,
+        data: int = -1,
+        prev: "ListNode | None" = None,
+        next: "ListNode | None" = None,
+    ) -> None:
+        self.key = key
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+
+class Lru:
+    def __init__(self, capacity) -> None:
+        self.capacity = capacity
+        self.nodes = {}
+        self.head = self.tail = ListNode()
+
+    def insert(self, key, data) -> None:
+        if len(self.nodes) == self.capacity:
+            lru_node = self.head.next
+            self.head.next = lru_node.next
+            if lru_node.next:
+                lru_node.next.prev = self.head
+            del self.nodes[lru_node.key]
+
+        self.tail.next = ListNode(key, data)
+        self.tail = self.tail.next
+        self.nodes[key] = self.tail
+
+    def lookup(self, key) -> int:
+        self.update(key)
+        return self.tail.data
+
+    def update(self, key) -> None:
+        node = self.nodes[key]
+        if node == self.tail:
+            return
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        self.tail.next = node
+        node.prev = self.tail
+        node.next = None
+
+        self.tail = node
+
+
 class LruCache:
     def __init__(self, capacity: int) -> None:
         # TODO - you fill in here.
